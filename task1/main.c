d4069ba22b3c996ff2f4cb746fc713d221a43e1b
457719079 <457719079@qq.com>
2022-10-25 21:56:44 +0800

    Modify main.c

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+#include<stdio.h>
+int main(){
+	double a=1;
+	double h=0.001;
+	double x=0;
+	double y=a;
+	double z;
+	printf("x        y\n");
+	printf("%f %f\n",x,y);
+	z=(1-h)*y;
+	while(y-z>=0.001*h){
+	x=x+h;
+	y=z;	
+	z=(1-h)*y;	
+	printf("%f %f\n",x,y);	
+	}
 	return 0;
 }
-
